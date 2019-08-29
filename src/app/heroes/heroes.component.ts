1de8c84bdae8f3cda70289a092aa1668fca9e253
Stanislav_Domanskyi <stas.domanskiy@gmail.com>
2019-08-29 14:00:50 +0300

    routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,14 +8,9 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  public selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { 
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
